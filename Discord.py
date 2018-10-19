317880f55ef1b2dbbe4000785bb5198b040c3ce1
hugo <hugo.meeldijk@hotmail.com>
2018-10-19 16:00:11 +0200

    My mom is retarted

--- Discord.py
+++ Discord.py
@@ -1,3 +1,11 @@
-def test():
-    print("hello")
-test()
+import discord
+import logging
+import asyncio
+from discord.ext import commands
+import requests
+import random
+import json
+import aiohttp
+import sqlite3
+import time
+import datetime
