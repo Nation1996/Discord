b4b8870a9a262e3edac5a60d60d3640e991b1514
Dii <diogo22ramos@gmail.com>
2018-10-19 16:19:27 +0100

    Added command hub

--- Discord.py
+++ Discord.py
@@ -1,10 +1,21 @@
 import discord
-import logging
-import asyncio
-import requests
-import random
-import json
-import aiohttp
-import sqlite3
-import time
-import datetime
+import youtube_dl as ytb_dl
+#id 502858151612383232
+client = discord.Client()
+token = open("token.txt", 'r').read()
+# pip install -U git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice]
+
+@client.event
+async def on_ready():
+    print("Logged In.")
+
+@client.event
+async def on_message(message):
+    msg = message.content.lower()
+    if msg.startswith(">"):
+        if msg[1] == "t":
+            await message.channel.send(f"Working")
+        if msg == ">quit":
+            print("Logged out")
+            await client.close()
+client.run(token)
